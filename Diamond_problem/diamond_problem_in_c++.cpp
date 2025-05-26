ac078041837bf87707d66529a27638d86d2373db
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-05-26 13:39:45 +0400

    Add files via upload

---
+++ Diamond_problem/diamond_problem_in_c++.cpp
@@ -0,0 +1,93 @@
+
+#include <iostream>
+#include <string>
+
+class Animal
+{
+public:
+    virtual void display() = 0;
+
+    Animal()
+    {
+        std::cout << "Animal constructor" << std::endl;
+    }
+
+    virtual ~Animal()
+    {
+        std::cout << "Animal destructor" << std::endl;
+    }
+};
+
+class Mammal : virtual public Animal
+{
+private:
+    std::string color;
+
+public:
+    void display()
+    {
+        std::cout << "display method from Mammal class" << std::endl;
+    }
+
+    Mammal()
+    {
+        std::cout << "Mammal constructor" << std::endl;
+    }
+
+    ~Mammal()
+    {
+        std::cout << "Mammal destructor" << std::endl;
+    }
+};
+
+class Reptile : virtual public Animal
+{
+private:
+    std::string color;
+
+public:
+    void display()
+    {
+        std::cout << "display method from Reptile class" << std::endl;
+    }
+
+    Reptile()
+    {
+        std::cout << "Reptile constructor" << std::endl;
+    }
+
+    ~Reptile()
+    {
+        std::cout << "Reptile destructor" << std::endl;
+    }
+};
+
+class Platypus : public Mammal, Reptile
+{
+private:
+    std::string color;
+
+public:
+    void display()
+    {
+        std::cout << "display method from Platypus class" << std::endl;
+    }
+
+    Platypus()
+    {
+        std::cout << "Platypus constructor" << std::endl;
+    }
+
+    ~Platypus()
+    {
+        std::cout << "Platypus destructor" << std::endl;
+    }
+};
+
+
+int main()
+{
+    Platypus ob;
+    ob.display();
+}
+
