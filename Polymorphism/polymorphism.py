e1c2feef2a2d436d9a8cd2a4ef13caa58f9e3e64
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-05-26 18:08:09 +0400

    Add files via upload

---
+++ Polymorphism/polymorphism.py
@@ -0,0 +1,27 @@
+#Polymorphism simple examples
+
+#example1
+a_int = 3
+b_int = 4
+c_int = a_int + b_int
+print(c_int)
+
+#example2
+a_str = "3"
+b_str = "4"
+c_str = a_str + b_str
+print(c_str)
+
+#example3
+input_list = [3, -4, 0, 2, 7]
+print(len(input_list))
+
+
+#example4
+input_str = "Yerevan"
+print(len(input_str))
+
+
+#example5
+input_dict = { 1: "a", 2: "b", 3: "c"}
+print(len(input_dict))
