56ca2f6d888c3a229ff60e7a30d496e03090ea0f
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-05-26 19:41:30 +0400

    Add files via upload

---
+++ Polymorphism/duck_typing.py
@@ -0,0 +1,35 @@
+class Dog:
+    def swim(self):
+        print("Dog is swimming")
+
+    def voice(self):
+        print("I am Dog")
+
+class Duck:
+    def swim(self):
+        print("Duck is swimming")
+
+    def voice(self):
+        print("I am Duck")
+
+
+class Fish:
+    def swim(self):
+        print("Fish is swimming")
+
+    def voice(self):
+        print("I am Fish")
+
+
+def display(obj):
+    obj.voice()
+    obj.swim()
+
+
+
+dog = Dog()
+display(dog)
+fish = Fish()
+display(fish)
+duck = Duck()
+display(duck)
