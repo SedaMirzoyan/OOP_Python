93160edd63161978dc446301da1dafe5d8aa381f
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-05-28 20:36:04 +0400

    Add files via upload

--- Polymorphism/duck_typing.py
+++ Polymorphism/duck_typing.py
@@ -1,13 +1,22 @@
 #Duck typing
 
 class Dog:
+    """
+    Dog class with swim and voice methods,
+    which print generic messages
+    """
     def swim(self):
         print("Dog is swimming")
 
     def voice(self):
         print("I am Dog")
 
+
 class Duck:
+    """
+    Duck class with swim and voice methods,
+    which print generic messages
+    """
     def swim(self):
         print("Duck is swimming")
 
@@ -16,6 +25,10 @@ class Duck:
 
 
 class Fish:
+    """
+    Fish class with swim and voice methods,
+    which print generic messages
+    """
     def swim(self):
         print("Fish is swimming")
 
@@ -24,11 +37,22 @@ class Fish:
 
 
 def display(obj):
+    """
+    This function does not care about actual type of an object,
+    only that it has voice and swim methods
+
+    Args:
+        obj (class object): obj
+    """
     obj.voice()
     obj.swim()
 
 
 
+"""
+create objects of dog, duck and fish classes and pass them to display function,
+for each call it will print corresponding message
+"""
 dog = Dog()
 display(dog)
 fish = Fish()
