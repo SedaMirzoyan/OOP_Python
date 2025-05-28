79cbb07fcc8a071e2b7502996e80584079119ae7
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-05-28 20:43:08 +0400

    Add files via upload

--- Polymorphism/method_overloading.py
+++ Polymorphism/method_overloading.py
@@ -1,10 +1,31 @@
 #Method overloading
 
 class A:
+    """
+    class which has add and mul overloaded methods
+    """
     def add(self, a, b, c=0):
+        """
+        Method overloading with the help of default argument c,
+        we can call it with 2 or 3 arguments
+
+        Args:
+            a (int): a
+            b (int): b
+            c (int): c
+        """
         return a + b + c
     
+    
     def mul(self, *args):
+        """
+        Method overloading with the help of default *args,
+        So we can pass any number of arguments
+
+        Args:
+            *args: any number of arguments
+        """
+
         product = 1
         for i in args:
             product = product * i
@@ -12,6 +33,11 @@ class A:
     
 
 
+"""
+create an object of A class,
+overloading with default argument for add method (for 2 or 3 arguments),
+and with any number of arguments for mul method
+"""
 a = A()
 print(a.add(5, 6))
 print(a.add(5, 6, 7))
