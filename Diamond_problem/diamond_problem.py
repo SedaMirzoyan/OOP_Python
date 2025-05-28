9665844e9bf5bc8215eb6d62e16b481ee608b411
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-05-28 20:15:28 +0400

    Add files via upload

--- Diamond_problem/diamond_problem.py
+++ Diamond_problem/diamond_problem.py
@@ -1,6 +1,10 @@
 #diamond problem
 
 class A:
+    """
+    Base A class, with display and show methods,
+    which prints generic message
+    """
     def display(self):
         print("display from A")
 
@@ -9,28 +13,64 @@ class A:
 
 
 class B(A):
+    """
+    B class derived from A class
+    """
+
     def display(self):
+        """
+        Prints generic message, overrides base display method
+        """
         print("display from B")
 
+
     def show(self):
+        """
+        Prints generic message, overrides base show method
+        """
         print("show from class B")    
 
+
 class C(A):
+    """
+    C class derived from A class
+    """
     def display(self):
+        """
+        Prints generic message, overrides base display method
+        """
         print("display from C")
 
+
     def show(self):
+        """
+        Prints generic message, overrides base show method
+        """
         print("show from class C")
 
+
 class D(B, C):
+    """
+    D class derived from both B and C classes
+    """
     #def display(self):
     #    print("display from D")
     
     def show(self):
+        """
+        calling C class's show method        
+        """
         #super().show()  #this one is calling B show method
         C.show(self)
 
+
+
+#create object of D class
 d = D()
+
+#B class's display will be called
 d.display()
 print(D.mro())
+
+#C class display will be called
 d.show()
