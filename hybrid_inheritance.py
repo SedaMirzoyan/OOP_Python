619a18c5fff3eb80928ba545ee529c444a5b7954
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-05-28 19:54:53 +0400

    Add files via upload

--- hybrid_inheritance.py
+++ hybrid_inheritance.py
@@ -1,27 +1,54 @@
 #Hybrid inheritance
 
 class A:
+    """
+    Base A class, which has display method.
+    prints a generic message in display method.
+    """
     def display(self):
         print("display from A")
 
+
 class B(A):
+    """
+    Base B class, which has display method,
+    prints a generic message in display method.
+    """
     def display(self):
         print("display from B")
 
 
 class C:
+    """
+    Base C class, which has display method,
+    prints a generic message in display method.
+    """
     def show(self):
         print("show from C")
 
 
 class D(B, C):
+    """
+    D class, which inherits from both B and C classes
+    """
     def display(self):
+        """
+        prints a generic message in display method.
+        """
         print("display from D")
 
+
     def show(self):
+        """
+        prints a generic message in show method.
+        """
         print("show from D")    
 
 
+
+"""
+create D class object, overrides show and display methods
+"""
 d = D()
 d.show()
 d.display()
